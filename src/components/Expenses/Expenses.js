ff3c6b280f2f9440c6b3cf1996a450605664d58d
Sanjay S <s.s@email.com>
2022-12-12 16:58:38 +0000

    sec05: Demo App - adding a chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [currentfilter, setCurrentFilter] = useState({
@@ -26,6 +27,7 @@ const Expenses = (props) => {
         selected={currentfilter}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
