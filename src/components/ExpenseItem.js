94eaa9f0cb33675f8ae06273f97a6f01905e87eb
Sanjay S <s.s@email.com>
2022-12-12 16:57:15 +0000

    sec03: More Complex JSX Code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>28th March 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>£294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
