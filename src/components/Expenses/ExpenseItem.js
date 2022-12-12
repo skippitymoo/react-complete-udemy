c08264d0b0a07d25e5b89e49a446b12820379695
Sanjay S <s.s@email.com>
2022-12-12 16:57:17 +0000

    sec04: How Component Functions Are Executed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,17 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log('yEEEEEooooooooo');
+    title = 'Updated!'; // this will change the variable value but the UI won't update
   };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>£{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
