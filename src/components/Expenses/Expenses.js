f5a93671284cafa4368f4922630cc3815d3a15c3
Sanjay S <s.s@email.com>
2022-12-12 16:58:37 +0000

    sec04: asignment 2: Events & State - MAX

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [expenses, setExpenses] = useState(props.expenses);
+  const [currentfilter, setCurrentFilter] = useState({
+    year: 2021,
+  });
+
+  const filterExpenses = (filter, expenses) => {
+    expenses.forEach((e) => {
+      e.visible = filter.year ? e.date.getFullYear() === filter.year : true;
+    });
+    setExpenses([...expenses]);
+  };
+
+  const filterChangeHandler = (event) => {
+    currentfilter.year = +event.target.value;
+    filterExpenses(currentfilter, expenses);
+    setCurrentFilter(currentfilter);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter
+        selected={currentfilter}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.expenses
         .filter((exp) => exp.visible)
         .map((exp) => {
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -7,7 +7,10 @@ const ExpensesFilter = (props) => {
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={props.onFilterChange}>
+        <select
+          defaultValue={props.selected.year}
+          onChange={props.onChangeFilter}
+        >
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
