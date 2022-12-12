678bb3f226fc363701c1e1e9796fd3868403c98e
Sanjay S <s.s@email.com>
2022-12-12 16:58:35 +0000

    sec03: spilt Componenets into Multiple Componenets

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const day = props.date.toLocaleString('en-GB', { day: '2-digit' });
-  const month = props.date.toLocaleString('en-GB', { month: 'long' });
-  const year = props.date.getFullYear();
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>£{props.amount}</div>
