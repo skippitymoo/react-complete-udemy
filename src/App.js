a3cb9ce1e4f38452434ab4edbb63cb948cbdccc9
Sanjay S <s.s@email.com>
2022-12-12 16:58:35 +0000

    sec03: assignment01 - component basics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -26,16 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expenses.map((exp) => {
-        return (
-          <ExpenseItem
-            title={exp.title}
-            amount={exp.amount}
-            date={exp.date}
-            key={exp.id}
-          ></ExpenseItem>
-        );
-      })}
+      <Expenses expenses={expenses} />
     </div>
   );
 }
