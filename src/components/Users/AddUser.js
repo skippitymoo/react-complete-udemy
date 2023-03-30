7603adfd416921877f878f444592111b8b6fa9e3
Sanjay S <s.s@email.com>
2023-03-30 12:52:30 +0100

    sec08: addUser, use two-way binding and hook to deal with form inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import "./AddUser.scss";
 
 const AddUser = () => {
+  const [usernameVal, setUserName] = useState("");
+  const [ageVal, setAge] = useState("");
+
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const data = new FormData(event.target);
-    const fieldsMissing = [...data.entries()].some((field) => !field[1]);
+    //const data = new FormData(event.target);
 
-    if (fieldsMissing) {
+    if (!usernameVal || !ageVal) {
       window.location.hash = "popup";
     } else {
-      event.target.reset();
+      setUserName("");
+      setAge("");
     }
+
+    // const fieldsMissing = [...data.entries()].some((field) => !field[1]);
+
+    // if (fieldsMissing) {
+    //   window.location.hash = "popup";
+    // } else {
+    //   event.target.reset();
+    // }
   };
 
   return (
@@ -25,11 +37,20 @@ const AddUser = () => {
           type="text"
           name="username"
           id="username"
+          value={usernameVal}
+          onChange={(e) => setUserName(e.target.value)}
         />
         <label className="form__label" htmlFor="age">
           Age (Years)
         </label>
-        <input className="form__input" type="number" name="age" id="age" />
+        <input
+          className="form__input"
+          type="number"
+          name="age"
+          id="age"
+          value={ageVal}
+          onChange={(e) => setAge(e.target.value)}
+        />
         <button className="btn form__button add-user__button">Add User</button>
       </form>
     </div>
