03919db618c5afe7e7d1da430a864a1ac3b77100
Sanjay S <s.s@email.com>
2023-04-27 16:32:52 +0100

    sec11: initial clear down

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import { useContext } from "react";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
-
-function App() {
-  const authCtx = useContext(AuthContext);
-  return (
-    <>
-      <MainHeader />
-      <main>
-        {!authCtx.isLoggedIn && <Login />}
-        {authCtx.isLoggedIn && <Home />}
-      </main>
-    </>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { AuthContextProvider } from "./store/auth-context";
-
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>
-);
+root.render(<App />);
