2a0d1f7f33efdf7b02afebd21c52cfed4adf243b
skippitymoo <s.s@email.com>
2024-02-12 14:07:02 +0000

    sec12: Mock Service Worker for Node - use msw in MainContent test

--- src/components/layout/MainContent.test.tsx
+++ src/components/layout/MainContent.test.tsx
@@ -1,28 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import { MainContent } from './MainContent';
 import { axe, toHaveNoViolations } from 'jest-axe';
+import { server } from '../../shared/mocks/msw/server';
 
 expect.extend(toHaveNoViolations);
 
 describe('MainContent', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   it('has no accessibility violations', async () => {
     const { container } = render(<MainContent />);
 
-    expect(await axe(container)).toHaveNoViolations();
+    await screen.findByText('Schnitzel');
+
+    expect(await axe(container!)).toHaveNoViolations();
   });
 
-  it('should render with the correct class name', () => {
+  it('should render with the correct class name', async () => {
     render(<MainContent />);
 
-    const main = screen.getByRole('main');
+    const main = await screen.findByRole('main');
 
     expect(main).toHaveClass('main-container');
   });
 
-  it('should render image', () => {
+  it('should render image', async () => {
     render(<MainContent />);
 
-    const main = screen.getByRole('img');
+    const main = await screen.findByRole('img');
 
     expect(main).toHaveClass('bg-image__image');
   });
